c9a8fc526152c247cf89fcff5b76e964945396ef
gsofter <real.kingstar@outlook.com>
2021-08-24 10:31:46 -0500

    update deploy config

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,9 +3,9 @@ const MultiSig = artifacts.require("MultiSig");
 const MoldNFT = artifacts.require("MoldNFT");
 const AuctionSea = artifacts.require("AuctionSea");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(MultiSig);
-  deployer.deploy(MoldNFT);
-  deployer.deploy(AuctionSea);
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(MultiSig);
+  await deployer.deploy(MoldNFT);
+  await deployer.deploy(AuctionSea);
 };
